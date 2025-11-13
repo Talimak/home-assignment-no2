4796dc07b19396ab8e6954b571a7ffedc0249d88
tali mak <talimak2002@gmail.com>
2025-11-13 13:45:48 +0200

    Fix: converted client from submodule to normal folder

---
+++ client/main.js
@@ -0,0 +1,11 @@
+// const { createElement } = require("react");
+
+// const container = document.querySelector("#movies_container");
+
+// movies.forEach(movie =>{
+// let div = document.createElement('div');
+// let title = document.createElement('h3');
+// title.textContent=`${movie.title} (${movie.year})`;
+// div.appendChild title;
+// }
+// )
